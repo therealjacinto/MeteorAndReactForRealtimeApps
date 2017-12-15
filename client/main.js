154aeafbbf121851319d1eb121c01ac98405aed5
Jacinto Dominguez <jacinto@kaiamcorp.com>
2017-12-14 17:59:42 -0800

    End of Section 2, Lecture 9 (Building a React Component)

--- client/main.js
+++ client/main.js
@@ -4,6 +4,12 @@
 import React from 'react';
 
 // Create a component
-
+const App = () => {
+    return (
+        <div>
+            React App #2
+        </div>
+    );
+};
 
 // Render this component to the screen
