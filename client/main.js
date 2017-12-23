210e489ef60ea0c1629bc5a345a381cf9bcf0160
Jacinto Dominguez <jacinto@kaiamcorp.com>
2017-12-22 16:43:11 -0800

    End of Section 2, Lecture 19 (AJAX with Axios)

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ImageList from './components/image_list';
+import axios from 'axios';
 
 // Create a component
 const App = () => {
@@ -17,4 +18,6 @@ const App = () => {
 // Render this component to the screen
 Meteor.startup(() => {
     ReactDOM.render(<App />, document.querySelector('.container'));
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+        .then(response => console.log(response));
 });
