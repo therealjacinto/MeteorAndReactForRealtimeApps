a69920af81e59fd4ab564ee64b9fb8eea43809f3
Jacinto Dominguez <jacinto@kaiamcorp.com>
2018-01-08 18:55:13 -0800

    End of Section 3, Lecture 52 (Refactoring from Functions to Classes)

--- employees/client/components/employee_list.js
+++ employees/client/components/employee_list.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
 const PER_PAGE = 20;
 
-const EmployeeList = (props) => {
-    // props.employees => an array of employee objects
-
-    return (
-        <div>
-            <div className="employee-list">
-                {props.employees.map(employee =>
-                    <EmployeeDetail key={employee._id} employee={employee} />)}
+class EmployeeList extends Component {
+    render() {
+        // props.employees => an array of employee objects
+        return (
+            <div>
+                <div className="employee-list">
+                    {this.props.employees.map(employee =>
+                        <EmployeeDetail key={employee._id} employee={employee}/>)}
+                </div>
+                <button onClick={() => Meteor.subscribe('employees', 40)}
+                        className="btn btn-primary">
+                    Load More...
+                </button>
             </div>
-            <button onClick={() => Meteor.subscribe('employees', 40)}
-                    className="btn btn-primary">
-                Load More...
-            </button>
-        </div>
-    );
+        );
+    }
 };
 
 export default createContainer(() => {
