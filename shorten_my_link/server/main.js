0c367cf4ccdc9e9f13e436aaf06212e07fbf35f9
Jacinto Dominguez <jacinto@kaiamcorp.com>
2018-01-16 15:10:50 -0800

    End of Section 4, Lecture 72 (Redirecting Users to Long Links)

--- shorten_my_link/server/main.js
+++ shorten_my_link/server/main.js
@@ -9,8 +9,18 @@ Meteor.startup(() => {
   });
 });
 
+// Executed whenever a user visits with a route like 'localhost:3000/abcd'
+function onRoute(req, res, next) {
+    // Take the token out of the url and try to find a matching link in the Links collection
+    const link = Links.findOne({ token: req.params.token });
+
+    // If we find a link object, redirect the user to the long URL
+
+    // If we don't find a link object, send the user to the normal React app
+}
+
 const middleware = ConnectRoute(function(router) {
-    router.get('/:token', (req) => console.log(req));
+    router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
