d68921fad087870bc761e8caa339d6bd996d8fa6
Jacinto Dominguez <jacinto@kaiamcorp.com>
2018-01-26 18:58:39 +0000

    End of Section 5, Lecture 85 (Create Bin Button)

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import App from './components/app';
+import { Bins } from '../imports/collections/bins';
 
 Meteor.startup(() => {
     ReactDom.render(<App />, document.querySelector('.render-target'));
