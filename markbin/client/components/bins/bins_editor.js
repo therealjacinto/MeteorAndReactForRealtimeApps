78026b19db410e106a5204963ab8d898845e8acd
Jacinto Dominguez <jacinto@kaiamcorp.com>
2018-01-27 11:21:54 +0000

    End of Section 5, Lecture 98 (Bin Editor)

---
+++ markbin/client/components/bins/bins_editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BinsEditor extends Component {
+    render() {
+        return(
+            <div>BinsEditor</div>
+        );
+    }
+}
+
+export default BinsEditor;
