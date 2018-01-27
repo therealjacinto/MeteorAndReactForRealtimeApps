2e200edf67e9dbfd9b6929b2bee6bf7f8b8a01fe
Jacinto Dominguez <jacinto@kaiamcorp.com>
2018-01-27 10:39:47 +0000

    End of Section 5, Lecture 90 (Routing with React Router)

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
 import { Bins } from '../imports/collections/bins';
 
+const routes = (
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+        </Route>
+    </Router>
+);
+
 Meteor.startup(() => {
-    ReactDom.render(<App />, document.querySelector('.render-target'));
+    ReactDom.render(routes, document.querySelector('.render-target'));
 });
