223a729eca4d421217f0ab49e3da0e166413c143
Jacinto Dominguez <jacinto@kaiamcorp.com>
2018-01-27 10:14:57 +0000

    End of Section 5, Lecture 89 (Removing a Bin)

--- markbin/client/components/bins/bins_list.js
+++ markbin/client/components/bins/bins_list.js
@@ -3,17 +3,26 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 
 class BinsList extends Component {
+    onBinRemove(bin) {
+        Meteor.call('bins.remove', bin);
+    }
+
     renderList() {
         return this.props.bins.map(bin => {
             return (
                 <li className="list-group-item" key={bin._id}>
                     Bin {bin._id}
+                    <span className="pull-right">
+                        <button className="btn btn-danger" onClick={() => this.onBinRemove(bin)}>
+                            Remove
+                        </button>
+                    </span>
                 </li>
             );
         });
     }
 
-    render () {
+    render() {
         return (
             <ul className="list-group">
                 {this.renderList()}
