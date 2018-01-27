6f10050dfada27427a3c74ce50b23232cef51bd9
Jacinto Dominguez <jacinto@kaiamcorp.com>
2018-01-27 12:41:51 +0000

    End of Section 5, Lecture 107 (Rendering Shared Addresses)

--- markbin/client/components/bins/bins_share.js
+++ markbin/client/components/bins/bins_share.js
@@ -6,6 +6,14 @@ class BinsShare extends Component {
         Meteor.call('bins.share', this.props.bin, email)
     }
 
+    renderShareList() {
+        return this.props.bin.sharedWith.map(email => {
+            return <button
+                key={email}
+                className="btn btn-default">{email}</button>
+        })
+    }
+
     render() {
         return(
             <footer className="bins-share">
@@ -16,6 +24,12 @@ class BinsShare extends Component {
                             Share Bin
                         </button>
                     </div>
+                </div>
+                <div>
+                    Shared With:
+                </div>
+                <div className="btn-group">
+                    {this.renderShareList()}
                 </div>
             </footer>
         );
